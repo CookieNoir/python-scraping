32c26a7dd68539f40c14db2f4947aa1ebe0945ed
Nikolay Chernyshov <hornushero@gmail.com>
2022-11-15 18:44:16 +0300

    Added protection from non-parseable urls

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ import scraping
 if __name__ == '__main__':
     scraping.scrape(r"https://intehros.ru/",
                     r"C:\Workshop\Test Folder",
-                    2)
+                    3)
--- scraping.py
+++ scraping.py
@@ -1,3 +1,4 @@
+import urllib3.exceptions
 from bs4 import BeautifulSoup, SoupStrainer
 import requests
 from timeit import timeit
@@ -25,37 +26,38 @@ def scrape(from_url: str, to_directory_path: str, target_nesting_level: int = 1)
         if target_url in visited_urls:
             return
 
-        page = requests.get(target_url)
-        visited_urls.add(target_url)
-        if page.status_code != 200:
-            return
+        try:
+            page = requests.get(target_url)
+            if page.status_code == 200:
+                path = PurePosixPath(unquote(parsed_url.path))
+                directory_parts = list(path.parts[1:])
+                if parsed_url.params != "":
+                    directory_parts.append(parsed_url.params)
+                if parsed_url.query != "":
+                    directory_parts.append(parsed_url.query)
+                if parsed_url.fragment != "":
+                    directory_parts.append(parsed_url.fragment)
+                if len(directory_parts) > 0:
+                    filename = directory_parts[-1] + ".html"
+                else:
+                    filename = "index.html"
+                directory_path = netloc_directory
+                for part in directory_parts:
+                    directory_path = os.path.join(directory_path, part)
+                if not os.path.exists(directory_path):
+                    os.makedirs(directory_path)
+                full_path = os.path.join(directory_path, filename)
+                with open(full_path, "wb") as file:
+                    file.write(page.content)
 
-        path = PurePosixPath(unquote(parsed_url.path))
-        directory_parts = list(path.parts[1:])
-        if parsed_url.params != "":
-            directory_parts.append(parsed_url.params)
-        if parsed_url.query != "":
-            directory_parts.append(parsed_url.query)
-        if parsed_url.fragment != "":
-            directory_parts.append(parsed_url.fragment)
-        if len(directory_parts) > 0:
-            filename = directory_parts[-1]
-        else:
-            filename = "index.html"
-
-        directory_path = netloc_directory
-        for part in directory_parts:
-            directory_path = os.path.join(directory_path, part)
-        if not os.path.exists(directory_path):
-            os.makedirs(directory_path)
-        full_path = os.path.join(directory_path, filename)
-        with open(full_path, "wb") as file:
-            file.write(page.content)
-
-        if current_nesting_level < target_nesting_level:
-            for link in BeautifulSoup(page.content, "html.parser", parse_only=SoupStrainer('a')):
-                if link.has_attr('href'):
-                    _scrape_recursive(target_url, link['href'], current_nesting_level + 1)
+                if current_nesting_level < target_nesting_level:
+                    for link in BeautifulSoup(page.content, "html.parser", parse_only=SoupStrainer('a')):
+                        if link.has_attr('href'):
+                            _scrape_recursive(target_url, link['href'], current_nesting_level + 1)
+        except urllib3.exceptions.LocationParseError:
+            print(f"Couldn't parse URL {target_url}")
+        finally:
+            visited_urls.add(target_url)
 
     _prepare_directory()
     start_netloc = urlparse(from_url).netloc
